b47ef2d7b6438d3a92b90d284735983bd1789006
therter <thorsten.herter@cismet.de>
2014-08-08 13:48:09 +0200

    initial commit

---
+++ src/main/java/de/cismet/watergisserver/cidslayer/FgBaCidsLayer.java
@@ -0,0 +1,34 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.watergisserver.cidslayer;
+
+import Sirius.server.middleware.types.MetaClass;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class FgBaCidsLayer extends FgBakAeCidsLayer {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new FgBaCidsLayer object.
+     *
+     * @param  mc  DOCUMENT ME!
+     */
+    public FgBaCidsLayer(final MetaClass mc) {
+        super(mc);
+    }
+}
---
+++ src/main/java/de/cismet/watergisserver/trigger/FgBakAeTrigger.java
@@ -0,0 +1,133 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.watergisserver.trigger;
+
+import Sirius.server.newuser.User;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.sql.Statement;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.cids.trigger.AbstractDBAwareCidsTrigger;
+import de.cismet.cids.trigger.CidsTrigger;
+import de.cismet.cids.trigger.CidsTriggerKey;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = CidsTrigger.class)
+public class FgBakAeTrigger extends AbstractDBAwareCidsTrigger {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final transient org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(
+            FgBakAeTrigger.class);
+    private static final String FG_BAK_CLASS_NAME = "de.cismet.cids.dynamics.dlm25w.fg_bak_ae";
+    private static final String FG_BAK_TABLE_NAME = "dlm25w.fg_bak_ae";
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void afterDelete(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void afterInsert(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void afterUpdate(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void beforeDelete(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void beforeInsert(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void beforeUpdate(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public CidsTriggerKey getTriggerKey() {
+        return new CidsTriggerKey(CidsTriggerKey.ALL, FG_BAK_TABLE_NAME);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   o  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    @Override
+    public int compareTo(final CidsTrigger o) {
+        return 0;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   cidsBean  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private boolean isFgBakObject(final CidsBean cidsBean) {
+        return (cidsBean.getClass().getName().equals(FG_BAK_CLASS_NAME));
+    }
+
+    @Override
+    public void afterCommittedInsert(final CidsBean cidsBean, final User user) {
+    }
+
+    @Override
+    public void afterCommittedUpdate(final CidsBean cidsBean, final User user) {
+        restat(cidsBean, user);
+    }
+
+    @Override
+    public void afterCommittedDelete(final CidsBean cidsBean, final User user) {
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  cidsBean  DOCUMENT ME!
+     * @param  user      DOCUMENT ME!
+     */
+    private void restat(final CidsBean cidsBean, final User user) {
+        if (isFgBakObject(cidsBean)) {
+            try {
+                final long start = System.currentTimeMillis();
+                final Object id = cidsBean.getProperty("bak_st.von.route.id");
+                final Statement s = getDbServer().getActiveDBConnection().getConnection().createStatement();
+                // refresh fg_ba
+                s.execute("select dlm25w.import_fg_ba_by_fg_bak(" + id.toString() + ", '" + user.getName() + "')");
+                // refresh the stations on fg_ba
+                s.execute("select dlm25w.replace_fg_ba_by_fg_bak(" + id.toString() + ")");
+                // refresh fg_lak
+                s.execute("select dlm25w.import_fg_lak_by_fg_bak(" + id.toString() + ", '" + user.getName() + "')");
+                // refresh the stations on fg_lak
+                s.execute("select dlm25w.replace_fg_lak_by_fg_bak(" + id.toString() + ")");
+                // refresh fg_la
+                s.execute("select dlm25w.import_fg_la_by_fg_bak(" + id.toString() + ", '" + user.getName() + "')");
+                log.error("time to update stations " + (System.currentTimeMillis() - start));
+            } catch (Exception e) {
+                log.error("Error while executing fgsk trigger.", e);
+            }
+        }
+    }
+}
