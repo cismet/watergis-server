1e534e7c59bf4fa19d42454f4c4865c1c196b873
therter <thorsten.herter@cismet.de>
2017-06-28 20:31:25 +0200

    #7 checks which uses server searches intead of client queries

---
+++ src/main/java/de/cismet/cids/custom/watergis/server/search/OverlappedProf.java
@@ -0,0 +1,54 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.watergis.server.search;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class OverlappedProf extends AbstractAnalyzeSearch {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new WkkSearch object.
+     *
+     * @param  owner  DOCUMENT ME!
+     * @param  ids    DOCUMENT ME!
+     */
+    public OverlappedProf(final String owner, final int[] ids) {
+        super(owner, ids);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    protected String createQuery(final String owner, final int[] ids) {
+        if ((owner == null) || owner.startsWith("lung")
+                    || owner.equalsIgnoreCase("administratoren")) {
+            return
+                "select id, geom, ww_gr, ba_cd, ba_st_von, ba_st_bis, laenge, fis_g_date, fis_g_user from dlm25w.select_overlapped_fg_ba_prof(null, "
+                        + createSqlString(ids)
+                        + ")";
+        } else {
+            return
+                "select id, geom, ww_gr, ba_cd, ba_st_von, ba_st_bis, laenge, fis_g_date, fis_g_user from dlm25w.select_overlapped_fg_ba_prof('"
+                        + owner
+                        + "', "
+                        + createSqlString(ids)
+                        + ")";
+        }
+    }
+}
