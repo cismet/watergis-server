0219bfcfb01285775d46a20944194f86eff2c2a4
therter <thorsten.herter@cismet.de>
2017-06-28 20:46:25 +0200

    #7 fix problem tree search

--- src/main/java/de/cismet/cids/custom/watergis/server/search/RouteProblemsCount.java
+++ src/main/java/de/cismet/cids/custom/watergis/server/search/RouteProblemsCount.java
@@ -39,36 +39,37 @@ public class RouteProblemsCount extends AbstractCidsServerSearch {
     private static final transient Logger LOG = Logger.getLogger(RouteProblemsCount.class);
 
     public static final String DOMAIN_NAME = "DLM25W";
-    private static final String QUERY = "select coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidBakLinesByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_bak_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n"
-                + "+ coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidBaLinesByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_ba_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n"
-                + "+ coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidLakLinesByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_lak_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n"
-                + "+ coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidBakStationsByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_bak_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_bak_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n"
-                + "+ coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidBaStationsByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_ba_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_ba_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n"
-                + "+ coalesce(\n"
-                + "(select sum(cs_getNumberOfRows(invalidLakStationsByClass(cs_class.id, $user))) \n"
-                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_lak_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_lak_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
-                + "), 0\n"
-                + ")\n";
+//    private static final String QUERY = "select coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidBakLinesByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_bak_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n"
+//                + "+ coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidBaLinesByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_ba_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n"
+//                + "+ coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidLakLinesByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_lak_linie') and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n"
+//                + "+ coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidBakStationsByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_bak_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_bak_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n"
+//                + "+ coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidBaStationsByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_ba_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_ba_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n"
+//                + "+ coalesce(\n"
+//                + "(select sum(cs_getNumberOfRows(invalidLakStationsByClass(cs_class.id, $user))) \n"
+//                + "from cs_attr join cs_class on (cs_attr.class_id = cs_class.id) where foreign_key_references_to = (select id from cs_class where name = 'fg_lak_punkt') and cs_class.id not in ((select id from cs_class where name = 'fg_lak_linie')) and  (%1$s is null or ba.id = any(%1$s))\n"
+//                + "), 0\n"
+//                + ")\n";
+    private static final String QUERY = "select dlm25w.correction_count(%1$s, %2$s)";
 
     //~ Instance fields --------------------------------------------------------
 
@@ -96,9 +97,9 @@ public class RouteProblemsCount extends AbstractCidsServerSearch {
 
         if (ms != null) {
             try {
-                final String query = QUERY.replace("$user", owner);
                 final ArrayList<ArrayList> lists = ms.performCustomSearch(String.format(
-                            query,
+                            QUERY,
+                            owner,
                             SQLFormatter.createSqlArrayString(ids)));
                 return lists;
             } catch (RemoteException ex) {
