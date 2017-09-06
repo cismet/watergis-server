458a7fb0204184326a71a40595f52e8b8e825db4
therter <thorsten.herter@cismet.de>
2017-09-06 10:07:47 +0200

    #10 layer permission corrections

--- src/main/java/de/cismet/watergisserver/cidslayer/FgLaWkCidsLayer.java
+++ src/main/java/de/cismet/watergisserver/cidslayer/FgLaWkCidsLayer.java
@@ -12,6 +12,7 @@
 package de.cismet.watergisserver.cidslayer;
 
 import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.newuser.User;
 
 /**
  * DOCUMENT ME!
@@ -19,16 +20,25 @@ import Sirius.server.middleware.types.MetaClass;
  * @author   therter
  * @version  $Revision$, $Date$
  */
-public class FgLaWkCidsLayer extends WatergisDefaultCidsLayer {
+public class FgLaWkCidsLayer extends WatergisDefaultCidsLayer { // Default1505ConsideredCidsLayer {
 
     //~ Constructors -----------------------------------------------------------
 
     /**
      * Creates a new VwDvgStaluCidsLayer object.
      *
-     * @param  mc  DOCUMENT ME!
+     * @param  mc    DOCUMENT ME!
+     * @param  user  DOCUMENT ME!
      */
-    public FgLaWkCidsLayer(final MetaClass mc) {
+    public FgLaWkCidsLayer(final MetaClass mc, final User user) {
+//        super(
+//            mc,
+//            user,
+//            false,
+//            false,
+//            null,
+//            false,
+//            " left join dlm25w.k_ww_gr dlm25wPk_ww_gr1 on (dlm25w.fg_la_wk.ww_gr = dlm25wPk_ww_gr1.id)");
         super(mc);
     }
 }
