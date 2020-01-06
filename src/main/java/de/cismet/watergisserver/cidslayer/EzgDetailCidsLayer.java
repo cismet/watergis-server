906aba5b5cca1fa1ceb2bf6215cdbb648129220a
therter <thorsten.herter@cismet.de>
2020-01-06 15:52:09 +0100

    #28 add catalogues to ezg detail

--- src/main/java/de/cismet/watergisserver/cidslayer/EzgDetailCidsLayer.java
+++ src/main/java/de/cismet/watergisserver/cidslayer/EzgDetailCidsLayer.java
@@ -14,6 +14,8 @@ package de.cismet.watergisserver.cidslayer;
 import Sirius.server.middleware.types.MetaClass;
 import Sirius.server.newuser.User;
 
+import java.util.HashMap;
+
 /**
  * DOCUMENT ME!
  *
@@ -21,6 +23,21 @@ import Sirius.server.newuser.User;
  * @version  $Revision$, $Date$
  */
 public class EzgDetailCidsLayer extends WatergisDefaultCidsLayer {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final HashMap<String, String> CATALOGUE_NAME_MAP = new HashMap<String, String>();
+
+    static {
+        CATALOGUE_NAME_MAP.put("gbk_lawa", "gbk_lawa");
+        CATALOGUE_NAME_MAP.put("gbk_lawa_k", "gbk_lawa_k");
+        CATALOGUE_NAME_MAP.put("gbk_von", "gbk_von");
+        CATALOGUE_NAME_MAP.put("gbk_bis", "gbk_bis");
+        CATALOGUE_NAME_MAP.put("gbk_pl", "gbk_pl");
+        CATALOGUE_NAME_MAP.put("gbk_ordn", "gbk_ordn");
+        CATALOGUE_NAME_MAP.put("gwk_lawa", "la_cd");
+        CATALOGUE_NAME_MAP.put("gwk_gn", "la_gn");
+    }
 
     //~ Constructors -----------------------------------------------------------
 
@@ -31,6 +48,6 @@ public class EzgDetailCidsLayer extends WatergisDefaultCidsLayer {
      * @param  user  DOCUMENT ME!
      */
     public EzgDetailCidsLayer(final MetaClass mc, final User user) {
-        super(mc, user);
+        super(mc, CATALOGUE_NAME_MAP, user);
     }
 }
