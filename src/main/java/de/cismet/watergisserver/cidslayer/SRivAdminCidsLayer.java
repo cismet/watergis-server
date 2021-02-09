19d2092f4f7d449ec7913cdf086f9276a8013c84
therter <thorsten.herter@cismet.de>
2021-02-09 19:34:46 +0100

    fixed #32 add template cids layer

---
+++ src/main/java/de/cismet/watergisserver/cidslayer/SRivAdminCidsLayer.java
@@ -0,0 +1,36 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.watergisserver.cidslayer;
+
+import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.newuser.User;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class SRivAdminCidsLayer extends WatergisDefaultCidsLayer {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new VwDvgStaluCidsLayer object.
+     *
+     * @param  mc    DOCUMENT ME!
+     * @param  user  DOCUMENT ME!
+     */
+    public SRivAdminCidsLayer(final MetaClass mc, final User user) {
+        super(mc, user);
+    }
+}
