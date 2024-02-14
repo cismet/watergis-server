867bbe19050bb641c8e17301f4162d2b6555a376
therter <thorsten.herter@cismet.de>
2024-02-14 20:19:01 +0100

    #49 change srid to 5650

---
+++ src/main/java/de/cismet/cids/custom/helper/CrsHelper.java
@@ -0,0 +1,26 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.helper;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class CrsHelper {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final int SRID = 5650;
+}
