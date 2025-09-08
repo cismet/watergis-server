be6afd5680892f7800bfe65041203abdbf815097
therter <thorsten.herter@cismet.de>
2025-09-08 20:25:46 +0200

    #52 layer adjustments

---
+++ src/main/java/de/cismet/watergisserver/cidslayer/FgBakApsfrCidsLayer.java
@@ -0,0 +1,54 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.watergisserver.cidslayer;
+
+import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.newuser.User;
+
+import java.util.HashMap;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class FgBakApsfrCidsLayer extends Default1505ConsideredCidsLayer {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final HashMap<String, String> CATALOGUE_NAME_MAP = new HashMap<String, String>();
+
+    static {
+        CATALOGUE_NAME_MAP.put("apsfr_cd", "apsfr_cd");
+        CATALOGUE_NAME_MAP.put("apsfr_cdri", "apsfr_cd");
+    }
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new FgBaCidsLayer object.
+     *
+     * @param  mc    DOCUMENT ME!
+     * @param  user  DOCUMENT ME!
+     */
+    public FgBakApsfrCidsLayer(final MetaClass mc, final User user) {
+        super(
+            mc,
+            user,
+            false,
+            false,
+            CATALOGUE_NAME_MAP,
+            false,
+            " left join dlm25w.k_ww_gr dlm25wPk_ww_gr1 on (dlm25w.fg_bak.ww_gr = dlm25wPk_ww_gr1.id)");
+    }
+}
--- src/main/java/de/cismet/watergisserver/cidslayer/SgDetailCidsLayer.java
+++ src/main/java/de/cismet/watergisserver/cidslayer/SgDetailCidsLayer.java
@@ -43,4 +43,43 @@ public class SgDetailCidsLayer extends WatergisDefaultCidsLayer {
     public SgDetailCidsLayer(final MetaClass mc, final User user) {
         super(mc, CATALOGUE_NAME_MAP, user);
     }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getRestriction() {
+        if ((user == null)
+                    || user.getUserGroup().getName().equalsIgnoreCase("administratoren")
+                    || user.getUserGroup().getName().equalsIgnoreCase("seenprogramm")) {
+            return null;
+        } else {
+            return "vl = 0 and kg = 0";
+        }
+    }
+
+    @Override
+    protected boolean hasAttributeReadPermission(final String column, final User user) {
+        if (column.equals("dlm25w.sg_detail.vl") || column.equals("dlm25w.sg_detail.kg")) {
+            return ((user == null)
+                            || user.getUserGroup().getName().equalsIgnoreCase("administratoren")
+                            || user.getUserGroup().getName().equalsIgnoreCase("seenprogramm"));
+        } else {
+            return true;
+        }
+    }
+
+//    @Override
+//    protected String getFieldRestriction(final String column) {
+//        if (column.equals("dlm25w.sg_detail.vl") || column.equals("dlm25w.sg_detail.kg")) {
+//            if ((user == null)
+//                        || user.getUserGroup().getName().equalsIgnoreCase("administratoren")
+//                        || user.getUserGroup().getName().equalsIgnoreCase("seenprogramm")) {
+//                return null;
+//            } else {
+//                return "false";
+//            }
+//        }
+//
+//        return null;
+//    }
 }
