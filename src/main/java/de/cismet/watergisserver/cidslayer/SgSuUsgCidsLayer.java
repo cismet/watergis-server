c86275865ed4fd5081241b1f3ce439d2d219fb94
therter <thorsten.herter@cismet.de>
2025-10-01 19:12:18 +0200

    #52 permission adjustments

--- src/main/java/de/cismet/watergisserver/cidslayer/SgSuUsgCidsLayer.java
+++ src/main/java/de/cismet/watergisserver/cidslayer/SgSuUsgCidsLayer.java
@@ -33,4 +33,28 @@ public class SgSuUsgCidsLayer extends WatergisDefaultCidsLayer {
     public SgSuUsgCidsLayer(final MetaClass mc, final User user) {
         super(mc, true, user);
     }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getRestriction() {
+        if ((user == null)
+                    || user.getUserGroup().getName().equalsIgnoreCase("administratoren")
+                    || user.getUserGroup().getName().equalsIgnoreCase("seenprogramm")) {
+            return null;
+        } else {
+            return "usg_aktiv = 1";
+        }
+    }
+
+    @Override
+    protected boolean hasAttributeReadPermission(final String column, final User user) {
+        if (column.equals("dlm25w.sg_su_usg.usg_aktiv")) {
+            return ((user == null)
+                            || user.getUserGroup().getName().equalsIgnoreCase("administratoren")
+                            || user.getUserGroup().getName().equalsIgnoreCase("seenprogramm"));
+        } else {
+            return true;
+        }
+    }
 }
